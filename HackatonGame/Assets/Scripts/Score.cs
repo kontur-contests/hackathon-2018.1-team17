a4f7c6cfc7eaa91fcd271a1f48562a2dfddfff53
FoxWindler <gzahzah01@gmail.com>
2018-04-07 20:55:49 +0600

    rino+train

---
+++ HackatonGame/Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public int score = 0;
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		}
+	void OnTriggerEnter(Collider scoreTrigger)
+	{
+		if (scoreTrigger.tag == "ShortTrain") 
+			score = score + 10;
+			text.text = "score: " + score;
+
+	}
+}
