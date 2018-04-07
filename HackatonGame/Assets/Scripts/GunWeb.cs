b5609ab8bb0a106f0f2af9d68936f05b88ca5ff0
ntunin <nik.tyunin.92@mail.ru>
2018-04-08 05:18:47 +0600

    fix web

--- HackatonGame/Assets/Scripts/GunWeb.cs
+++ HackatonGame/Assets/Scripts/GunWeb.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class GunWeb : MonoBehaviour {
 
     public Vector3 targetVector;
+    public float timeToDestroy = 5;
+    private float lifeTime = 0;
+
 
     private void Update()
     {
         transform.position += targetVector * 0.1f;
+        lifeTime += Time.deltaTime;
+        if(lifeTime > timeToDestroy)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
