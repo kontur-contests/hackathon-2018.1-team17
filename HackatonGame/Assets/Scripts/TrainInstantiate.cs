a35d32d5f12cc487c5079f26d58e118f0c162a3a
ntunin <nik.tyunin.92@mail.ru>
2018-04-07 21:23:55 +0600

    sync

---
+++ HackatonGame/Assets/Scripts/TrainInstantiate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainInstantiate : MonoBehaviour {
+	
+	public GameObject trainTrigger;
+	public List <GameObject> trainList;
+	private Object trList;
+	// Use this for initialization
+	void Start () {
+		trList = trainList [Random.Range (0, 2)];
+		Instantiate (trList);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	/*void OnTriggerEnter(Collider trainTrigger){
+	Instantiate(longTrain);
+	}*/
+
+}
