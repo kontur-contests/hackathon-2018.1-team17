9194f56f5ada372b92828d9de75307bcee178b97
ntunin <nik.tyunin.92@mail.ru>
2018-04-08 08:59:34 +0600

    Jungle

---
+++ HackatonGame/Assets/Scripts/JungleScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JungleScript : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnEnter2D(Collision2D theCollision)
+    {
+        Debug.Log("Junglee!");
+        if (theCollision.gameObject.name.Contains("rhino"))
+        {
+            Debug.Log("You win!");
+        }
+    }
+}
