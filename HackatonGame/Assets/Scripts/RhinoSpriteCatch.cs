fca9bc2ae6b8135d9ad8b81ed0feed1a85104397
ptagus <ptagusss@ya.ru>
2018-04-08 08:10:00 +0600

    11

---
+++ HackatonGame/Assets/Scripts/RhinoSpriteCatch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RhinoSpriteCatch : MonoBehaviour {
+
+    public GameObject CatchTheRino;
+    public GameObject Area;
+	
+    public void ChangeIt()
+    {
+        Area.SetActive(false);
+        GetComponent<MeshRenderer>().enabled = false;
+        Instantiate(CatchTheRino, new Vector3(this.transform.position.x, this.transform.position.y+2, this.transform.position.z), Quaternion.identity);
+    }
+}
---
+++ HackatonGame/Assets/Scripts/UIController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    public GameObject WinScreen;
+    public GameObject LoseScreen;
+    public GameObject Pause;
+    public GameObject Panell;
+    public GameObject GO;
+
+	public void ToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void Win()
+    {
+        Panell.SetActive(true);
+        WinScreen.SetActive(true);
+        WinScreen.GetComponent<Animator>().SetTrigger("Show");
+        Time.timeScale = 0;
+        
+    }
+
+    public void Lose()
+    {
+        Panell.SetActive(true);
+        LoseScreen.SetActive(true);
+        LoseScreen.GetComponent<Animator>().SetBool("NBool",true);
+        Time.timeScale = 0;
+    }
+
+    public void ShowPause()
+    {
+        GO.GetComponent<SpeedRinoParent>().speed = 0;
+        Pause.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void STopPause()
+    {
+        GO.GetComponent<SpeedRinoParent>().speed = 0.04f;
+        Pause.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
